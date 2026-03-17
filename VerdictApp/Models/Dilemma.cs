a3894fcdf0fa59ae2a237faa162ad519e7c62f1b
Lee Rei Ern, Joy <ljo312342@gmail.com>
2026-03-17 18:33:06 +0100

    updated home page

--- VerdictApp/Models/Dilemma.cs
+++ VerdictApp/Models/Dilemma.cs
@@ -6,6 +6,7 @@ public class Dilemma {
     public string UserId { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public string Category { get; set; }
     public DateTime? ExpiresAt { get; set; }
     public DateTime CreatedAt { get; set; }
     public ApplicationUser User { get; set; }
