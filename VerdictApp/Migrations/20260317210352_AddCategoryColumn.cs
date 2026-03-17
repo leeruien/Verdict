24cd31bc884ff77eab7f2556d503084b6f9fc2d9
Lee Rei Ern, Joy <ljo312342@gmail.com>
2026-03-17 22:13:48 +0100

    changed to supabase hosting for db

---
+++ VerdictApp/Migrations/20260317210352_AddCategoryColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VerdictApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Dilemmas",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Dilemmas");
+        }
+    }
+}
